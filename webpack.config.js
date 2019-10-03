56edc00251cce87f60b32088e843b68c790c57ef
eessm01 <eessm01@gmail.com>
2019-10-02 19:31:49 -0500

    Configuracion del ambiente PlatziVideo

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    // guardar archivos resultantes cuando hagamos la compilacion
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js','.jsx'],
+    },
+    // reglas necesarias para el proyecto
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+            {
+                test: /\.(.s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
+        })
+    ]
+}
